df81b4c6e790e9ff57cb7a52a41f53f5affa64b6
Janos Erdos <janos.erdos@transferwise.com>
2020-01-26 18:13:00 +0000

    release v0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.erdos/erdos.yield "0.1.1"
+(defproject io.github.erdos/erdos.yield "0.1.2"
   :description "Generator/yield macro for clojure"
   :url "http://github.com/erdos/erdos.yield"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
